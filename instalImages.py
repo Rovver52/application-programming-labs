d5ed9c8ec5f6b5b8d2ff7ee55f2b9eae2c91aa16
Rovver52 <164315321+Rovver52@users.noreply.github.com>
2024-10-30 19:46:35 +0400

    Update instalImages.py

--- instalImages.py
+++ instalImages.py
@@ -1,34 +1,24 @@
-from icrawler.builtin import GoogleImageCrawler
 import os
-import csv
+from typing import Optional
+from icrawler.builtin import GoogleImageCrawler
 
-def install_images(keyword, imgdir, num):
+def install_images(keyword: str, imgdir: str, num: int) -> Optional[str]:
+    """
+    This function searches images by keyword in Google and downloads them to the directory. (Clears the directory every time the script is run).
+    :param keyword: keyword for searching
+    :param num: how many images you want to download
+    :param imgdir: directory to save images
+    """
     if not os.path.exists(imgdir):
         os.makedirs(imgdir)
 
     # Очистка директории
-    try:
-        for filename in os.listdir(imgdir):
-            os.remove(os.path.join(imgdir, filename))
-    except Exception as e:
-        print(f"Ошибка при очистке директории: {e}")
+    for filename in os.listdir(imgdir):
+        os.remove(os.path.join(imgdir, filename))
 
     try:
         google_crawler = GoogleImageCrawler(downloader_threads=4, storage={'root_dir': imgdir})
         google_crawler.crawl(keyword=keyword, max_num=num)
-
-        # Создание аннотации
-        annotation_file = os.path.join(imgdir, 'annotation.csv')
-        with open(annotation_file, 'w', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(['Absolute Path', 'Relative Path'])  # Заголовки
-
-            for filename in os.listdir(imgdir):
-                absolute_path = os.path.abspath(os.path.join(imgdir, filename))
-                relative_path = os.path.relpath(absolute_path, imgdir)
-                writer.writerow([absolute_path, relative_path])
-
-        return annotation_file
-
+        return imgdir
     except Exception as e:
         print(f"Произошла ошибка: {e}")
