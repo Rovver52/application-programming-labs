c82ad7aaaa6319001d8b8ec94e591f90de82ca03
Rovver52 <164315321+Rovver52@users.noreply.github.com>
2024-10-30 19:47:00 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 from instalImages import install_images
 from imageIterator import ImageIterator
-
+from annotation import create_annotation
 
 def main():
     parser = argparse.ArgumentParser(description='Download images and create an annotation CSV file.')
@@ -10,14 +10,25 @@ def main():
     parser.add_argument('num', type=int, help='Number of images to download (50-1000)', choices=range(50, 1001))
 
     args = parser.parse_args()
+    try:
 
-    annotation_file = install_images(args.keyword, args.imgdir, args.num)
-    print(f'Images downloaded and annotation saved to {annotation_file}')
+        imgdir = install_images(args.keyword, args.imgdir, args.num)
 
-    # Пример использования итератора
-    image_iterator = ImageIterator(annotation_file)
-    for absolute_path, relative_path in image_iterator:
-        print(f'Absolute: {absolute_path}, Relative: {relative_path}')
+        if imgdir:
+            print(f'Images downloaded to {imgdir}')
+
+            annotation_file = create_annotation(imgdir)
+
+            if annotation_file:
+                print(f'Annotation saved to {annotation_file}')
+
+            # Пример использования итератора
+                image_iterator = ImageIterator(annotation_file)
+                for absolute_path, relative_path in image_iterator:
+                    print(f'Absolute: {absolute_path}, Relative: {relative_path}')
+    except Exception as e:
+        print(f"Что-то пошло не так: {e} ")
+
 
 if __name__ == '__main__':
     main()
