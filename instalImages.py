6142dc4f2174f70261ea031c8e09160441379262
Rovver52 <164315321+Rovver52@users.noreply.github.com>
2024-11-02 14:41:51 +0400

    Update instalImages.py

--- instalImages.py
+++ instalImages.py
@@ -1,10 +1,9 @@
 import os
-from typing import Optional
 from icrawler.builtin import GoogleImageCrawler
 
-def install_images(keyword: str, imgdir: str, num: int) -> Optional[str]:
+def install_images(keyword: str, imgdir: str, num: int) -> None:
     """
-    This function searches images by keyword in Google and downloads them to the directory. (Clears the directory every time the script is run).
+    This function searches images by keyword in Google and downloads them to the directory. (Clears the directory every time the script is run)
     :param keyword: keyword for searching
     :param num: how many images you want to download
     :param imgdir: directory to save images
@@ -16,9 +15,5 @@ def install_images(keyword: str, imgdir: str, num: int) -> Optional[str]:
     for filename in os.listdir(imgdir):
         os.remove(os.path.join(imgdir, filename))
 
-    try:
-        google_crawler = GoogleImageCrawler(downloader_threads=4, storage={'root_dir': imgdir})
-        google_crawler.crawl(keyword=keyword, max_num=num)
-        return imgdir
-    except Exception as e:
-        print(f"Произошла ошибка: {e}")
+    google_crawler = GoogleImageCrawler(downloader_threads=4, storage={'root_dir': imgdir})
+    google_crawler.crawl(keyword=keyword, max_num=num)
