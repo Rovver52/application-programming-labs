4f321b02d9677f0562a2932cbbf3a4b8adae32fa
Rovver52 <164315321+Rovver52@users.noreply.github.com>
2024-11-04 21:26:33 +0400

    Update imageIterator.py

--- imageIterator.py
+++ imageIterator.py
@@ -2,6 +2,7 @@ import csv
 
 class ImageIterator:
     def __init__(self, annotation_file) -> None:
+        self.counter = 0
         self.annotation_file = annotation_file
         self.images = self.load_images()
 
@@ -21,3 +22,11 @@ class ImageIterator:
 
     def __iter__(self) -> iter:
         return iter(self.images)
+
+    def __next__(self):
+        if self.counter < len(self.images):
+            result = self.images[self.counter]
+            self.counter += 1
+            return result
+        else:
+            raise StopIteration
