344852a8c7070d441d27aba16ee6e76f3a7c3597
Rovver52 <164315321+Rovver52@users.noreply.github.com>
2024-11-13 17:13:25 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import argparse
 
-from worktofile import load_image, print_image_size, plot_histogram, invert_colors, display_images, save_image
+from worktofile import load_image, print_image_size, plot_histogram, invert_colors, display_images, save_image, print_histogram
 
 
-def main(input_image_path: str, output_image_path: str) -> None:
+def main() -> None:
     """
     Основная функция обработки изображения.
-
-    :param input_image_path: Путь к входному изображению.
-    :param output_image_path: Путь для сохранения инвертированного изображения.
     """
+    parser = argparse.ArgumentParser(description="Обработка изображения: инверсия цветов и построение гистограммы.")
+
+    parser.add_argument("input_image", help="Путь к входному изображению")
+    parser.add_argument("output_image", help="Путь для сохранения инвертированного изображения")
+
+    args = parser.parse_args()
+    input_image_path, output_image_path = args.input_image, args.output_image
     try:
 
         image = load_image(input_image_path)
         print_image_size(image)
         plot_histogram(image)
+        print_histogram()
         inverted_image = invert_colors(image)
         display_images(image, inverted_image)
         save_image(output_image_path, inverted_image)
@@ -23,11 +28,4 @@ def main(input_image_path: str, output_image_path: str) -> None:
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Обработка изображения: инверсия цветов и построение гистограммы.")
-
-    parser.add_argument("input_image", help="Путь к входному изображению")
-    parser.add_argument("output_image", help="Путь для сохранения инвертированного изображения")
-
-    args = parser.parse_args()
-
-    main(args.input_image, args.output_image)
+    main()
