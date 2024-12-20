11b721011dcf2ffb92f8a78b883886be84e279e3
Rovver52 <164315321+Rovver52@users.noreply.github.com>
2024-12-21 02:11:27 +0400

    Update imageiterator.py

--- lab6/imageiterator.py
+++ lab6/imageiterator.py
@@ -1,21 +1,18 @@
 import csv
 
-class ImageIterator:
-    def __init__(self, annotation_file: str) -> None:
-        """
-        Инициализирует итератор изображений.
 
-        :param annotation_file: Путь к файлу аннотации (CSV), содержащему пути к изображениям.
-        """
+
+class ImageIterator:
+    def __init__(self, annotation_file) -> None:
         self.counter = 0
         self.annotation_file = annotation_file
         self.images = self.load_images()
 
     def load_images(self) -> list[tuple[str, ...]]:
         """
-        Загружает изображения из файла аннотации.
-
-        :return: Список кортежей, представляющих строки из файла аннотации.
+        Collect filenames of downloaded images.
+        :param self: Parameter self of imageIterator
+        :return: list of filenames in this directory
         """
         images = []
         with open(self.annotation_file, 'r') as file:
@@ -26,20 +23,9 @@ class ImageIterator:
         return images
 
     def __iter__(self) -> iter:
-        """
-        Возвращает итератор для изображений.
-
-        :return: Итератор по списку изображений.
-        """
         return iter(self.images)
 
-    def next(self) -> tuple[str, ...]:
-        """
-        Возвращает следующую строку с изображением из списка.
-
-        :return: Кортеж с данными следующего изображения.
-        :raises StopIteration: Если больше нет изображений для отображения.
-        """
+    def __next__(self) -> tuple[str, ...]:
         if self.counter < len(self.images):
             result = self.images[self.counter]
             self.counter += 1
